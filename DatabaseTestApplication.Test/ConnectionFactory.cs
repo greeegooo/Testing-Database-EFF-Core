e23437f686b7084242017232e46886fac4799a81
greeegooo <gregomicha@gmail.com>
2019-09-23 23:45:50 -0300

    InMemory and Sqlite packages + implementación ConnectionFactory

--- DatabaseTestApplication.Test/ConnectionFactory.cs
+++ DatabaseTestApplication.Test/ConnectionFactory.cs
@@ -1,10 +1,65 @@
-﻿using System;
+﻿using DatabaseTestApplication.Model;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DatabaseTestApplication.Test
 {
-    class ConnectionFactory
+    class ConnectionFactory : IDisposable
     {
+        private bool disposedValue = false;
+
+        public BlogDBContext CreateContextForInMemory()
+        {
+            var option = new DbContextOptionsBuilder<BlogDBContext>()
+                .UseInMemoryDatabase(databaseName: "Test_Database").Options;
+
+            var context = new BlogDBContext(option);
+
+            if(context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+
+        public BlogDBContext CreateContextForSQLite()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var option = new DbContextOptionsBuilder<BlogDBContext>().UseSqlite(connection).Options;
+
+            var context = new BlogDBContext(option);
+
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                }
+
+                disposedValue = true;
+            }
+        }
     }
 }
