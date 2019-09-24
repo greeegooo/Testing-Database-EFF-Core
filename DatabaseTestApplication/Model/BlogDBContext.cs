9e523ae85c4f1d3fb2c11175c13f3132b2f26217
greeegooo <gregomicha@gmail.com>
2019-09-23 22:52:17 -0300

    Initial commit

---
+++ DatabaseTestApplication/Model/BlogDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseTestApplication.Model
+{
+    public partial class BlogDBContext : DbContext
+    {
+        public BlogDBContext()
+        {
+        }
+
+        public BlogDBContext(DbContextOptions<BlogDBContext> options) : base(options) { }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+    }
+}
